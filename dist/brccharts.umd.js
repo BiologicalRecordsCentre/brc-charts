60e05566fbc465a49884e91719f53a9ccc3e5e90
Richard Burkmar <rburkmar@ceh.ac.uk>
2021-10-26 13:53:51 +0100

    Building v0.4.6

--- dist/brccharts.umd.js
+++ dist/brccharts.umd.js
@@ -4827,7 +4827,7 @@
   }
 
   var name = "brc-d3";
-  var version = "0.4.5";
+  var version = "0.4.6";
   var description = "Javscript library for various D3 visualisations of biological record data.";
   var type = "module";
   var main = "dist/brccharts.umd.js";
